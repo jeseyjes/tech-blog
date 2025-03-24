f99734a7ef06f4009179c153f1657526b962f98b
jeseyjes <70363075+jeseyjes@users.noreply.github.com>
2025-03-24 14:26:21 +0300

    Update acm.tf

--- Terraform/acm.tf
+++ Terraform/acm.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "acm_certificate" {
   provider          = aws.secondary
-  domain_name       = "techitblog.cloudtalents.io"
-  alidation_method = "DNS"
+  domain_name       = "jeseyjess.cloudtalents.io"
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
